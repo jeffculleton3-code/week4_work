51c8329f62e00de1f40bc7243ed027237e5f29c3
jeffculleton3-code <jeff.culleton3@mail.dcu.ie>
2026-04-07 20:22:33 +0100

    Update send_type_test.c

--- send_type_test.c
+++ send_type_test.c
@@ -59,9 +59,6 @@ void client_task(int my_rank, int uni_size)
         send_message = dest = tag = 0;
         count = 1;
 
-		//Request for Isend
-		MPI_Request request;
-
         // sets the destination for the message
         dest = 0; // destination is root
 
@@ -69,7 +66,7 @@ void client_task(int my_rank, int uni_size)
         send_message = my_rank * 10;
 
         // sends the message
-        MPI_Isend(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD, &request);
+        MPI_Isend(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD, &status);
 		
 				
         // prints the message from the sender
