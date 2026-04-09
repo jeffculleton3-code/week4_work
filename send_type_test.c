03cfe9de448b4c7acc572e66e47c5d4306e3ff2d
jeffculleton3-code <jeff.culleton3@mail.dcu.ie>
2026-04-09 16:27:25 +0100

    Update send_type_test.c

--- send_type_test.c
+++ send_type_test.c
@@ -95,17 +95,16 @@ int root_task(int uni_size)
 	recv_message = source = tag = 0;
 	count = 1;
 	MPI_Status status;
-
+	
+	// get start time
+	clock_t t; 
+    t = clock();
 	
         // iterates through all the other ranks
         for (int their_rank = 1; their_rank < uni_size; their_rank++)
         {
                 // sets the source argument to the rank of the sender
                 source = their_rank;
-
-				// get start time
-				clock_t t; 
-    			t = clock();
 
                 // receives the messages
                 MPI_Recv(&recv_message, count, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
@@ -115,12 +114,14 @@ int root_task(int uni_size)
                 printf("Hello, I am %d of %d. Received %d from Rank %d\n",
                                 my_rank, uni_size, recv_message, source);
 
-				//find end time
-				t = clock() - t;
-				double time_taken = ((double)t)/CLOCKS_PER_SEC;
-				printf("%d took %fs to send\n", 
-					my_rank, time_taken);
         }// end for (int their_rank = 1; their_rank < uni_size; their_rank++)
+
+	
+	//find end time
+	t = clock() - t;
+	double time_taken = ((double)t)/CLOCKS_PER_SEC;
+	printf("%d took %fs to send\n", 
+				my_rank, time_taken);
 	return 0;
 }
 
