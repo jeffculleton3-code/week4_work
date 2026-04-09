6126d5ab981d3b5d3d39b3aafa3e36a8d99899ec
jeffculleton3-code <jeff.culleton3@mail.dcu.ie>
2026-04-09 16:32:35 +0100

    Update pingpong.c

--- pingpong.c
+++ pingpong.c
@@ -3,7 +3,21 @@
 
 int main(int argc, char **argv)
 {
-  
+    int ierror = 0;
+
+    // declare and initialise rank and size varibles
+    int my_rank, uni_size;
+    my_rank = uni_size = 0;
+
+    // intitalise MPI
+    ierror = MPI_Init(&argc, &argv);
+
+    // gets the rank and world size
+    ierror = MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+    ierror = MPI_Comm_size(MPI_COMM_WORLD,&uni_size);
+
+	//set counter to 0
+    int count = 0;
 }
 
 int check_args(int argc, char **argv)
